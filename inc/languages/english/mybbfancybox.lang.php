00b6744d7a548574f782f04959375c0d56472ca5
Mark Vincent <admin@rantcentralforums.com>
2018-09-01 12:25:19 -0500

    Initiate Language System (#20)
    * Fixes #15
    
    * For #9

---
+++ inc/languages/english/mybbfancybox.lang.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * MyBB FancyBox - plugin for MyBB 1.8.x forum software
+ * 
+ * @package MyBB Plugin
+ * @author MyBB Group - Eldenroot & effone - <eldenroot@gmail.com>
+ * @copyright 2018 MyBB Group <http://mybb.group>
+ * @link <https://github.com/mybbgroup/MyBB_Fancybox>
+ * @version 0.3
+ * @license GPL-3.0
+ * 
+ */
+
+$l['mybbfancybox'] = 'MyBB Fancybox';
+
+$l['mybbfancybox_uploaded'] = 'Uploaded:';
