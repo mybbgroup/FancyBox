74d43ee4236740db2382701e60ff011399adf186
Eldenroot <778890+Eldenroot@users.noreply.github.com>
2021-01-21 21:41:52 +0100

    Version number update

--- inc/plugins/mybbfancybox.php
+++ inc/plugins/mybbfancybox.php
@@ -59,7 +59,7 @@ function mybbfancybox_info()
 		"website"		=> "https://github.com/mybbgroup/MyBB_Fancybox",
 		"author"		=> "MyBB Group (Eldenroot & Wildcard & effone & Laird)",
 		"authorsite"	=> "https://github.com/mybbgroup/MyBB_Fancybox",
-		"version"		=> "1.1.0",
+		"version"		=> "1.1.1",
 		"codename"		=> "mybbfancybox",
 		"compatibility" => "18*"
 	);
