d2a51cc3b2e99ad3c67dddf2c8b8ea2758454f8e
Sven <SvePu@users.noreply.github.com>
2022-11-19 19:34:39 +0100

    Full language support

--- UPLOAD/inc/languages/english/admin/mybbfancybox.lang.php
+++ UPLOAD/inc/languages/english/admin/mybbfancybox.lang.php
@@ -16,59 +16,59 @@ $l['mybbfancybox'] = 'MyBB FancyBox';
 $l['mybbfancybox_description'] = 'MyBB integration of FancyBox JavaScript library for presenting images in a fancy way. Fully responsive, touch-enabled, and customizable.';
 
 // Settings
-$l['mybbfancybox_settings_group_title'] = 'MyBB FancyBox Settings';
-$l['mybbfancybox_settings_group_description'] = 'Settings for MyBB FancyBox';
+$l['setting_group_mybbfancybox'] = 'MyBB FancyBox Settings';
+$l['setting_group_mybbfancybox_desc'] = 'Settings for MyBB FancyBox';
 
-$l['mybbfancybox_open_image_urls_title'] = 'Detect image URL links?';
-$l['mybbfancybox_open_image_urls_description'] = 'YES (default) to automatically detect links to images in posts and enable them to be viewed in the MyBB FancyBox modal, NO to disable';
+$l['setting_mybbfancybox_open_image_urls'] = 'Detect image URL links?';
+$l['setting_mybbfancybox_open_image_urls_desc'] = 'YES (default) to automatically detect links to images in posts and enable them to be viewed in the MyBB FancyBox modal, NO to disable';
 
-$l['mybbfancybox_allowed_extensions_title'] = 'Allowed image extensions';
-$l['mybbfancybox_allowed_extensions_description'] = 'Leave blank (default) to use the default image extensions (.jpg, .gif, .png, .jpeg, .bmp, .apng) or enter a comma separated list of extensions to allow.<br />(This setting has no affect if the above setting is set to NO)';
+$l['setting_mybbfancybox_allowed_extensions'] = 'Allowed image extensions';
+$l['setting_mybbfancybox_allowed_extensions_desc'] = 'Leave blank (default) to use the default image extensions (.jpg, .gif, .png, .jpeg, .bmp, .apng) or enter a comma separated list of extensions to allow.<br />(This setting has no affect if the above setting is set to NO)';
 
-$l['mybbfancybox_include_images_from_urls_into_gallery_title'] = 'Include images from URLs in the gallery?';
-$l['mybbfancybox_include_images_from_urls_into_gallery_description'] = 'YES (default) to include detected images from URLs in posts in the gallery, NO to disable';
+$l['setting_mybbfancybox_include_images_from_urls_into_gallery'] = 'Include images from URLs in the gallery?';
+$l['setting_mybbfancybox_include_images_from_urls_into_gallery_desc'] = 'YES (default) to include detected images from URLs in posts in the gallery, NO to disable';
 
-$l['mybbfancybox_protect_images_title'] = 'Protect images?';
-$l['mybbfancybox_protect_images_description'] = 'YES to disable right-click and use simple image protection, NO (default) to enable right-click';
+$l['setting_mybbfancybox_protect_images'] = 'Protect images?';
+$l['setting_mybbfancybox_protect_images_desc'] = 'YES to disable right-click and use simple image protection, NO (default) to enable right-click';
 
-$l['mybbfancybox_watermark_title'] = 'Display watermark in images?';
-$l['mybbfancybox_watermark_description'] = 'YES to enable watermark overlay for images, NO (default) to disable';
+$l['setting_mybbfancybox_watermark'] = 'Display watermark in images?';
+$l['setting_mybbfancybox_watermark_desc'] = 'YES to enable watermark overlay for images, NO (default) to disable';
 
-$l['mybbfancybox_watermark_image_title'] = 'Path to watermark image';
-$l['mybbfancybox_watermark_image_description'] = 'Define the path to your watermark image, default: images/mybbfancybox/watermark.png';
+$l['setting_mybbfancybox_watermark_image'] = 'Path to watermark image';
+$l['setting_mybbfancybox_watermark_image_desc'] = 'Define the path to your watermark image, default: images/mybbfancybox/watermark.png';
 
-$l['mybbfancybox_watermark_low_resolution_images_title'] = 'Display watermark for low resolution images?';
-$l['mybbfancybox_watermark_low_resolution_images_description'] = 'YES to enable watermark overlay for small resolution images, NO (default) to disable<br />(This setting has no affect if the above setting is set to NO)';
+$l['setting_mybbfancybox_watermark_low_resolution_images'] = 'Display watermark for low resolution images?';
+$l['setting_mybbfancybox_watermark_low_resolution_images_desc'] = 'YES to enable watermark overlay for small resolution images, NO (default) to disable<br />(This setting has no affect if the above setting is set to NO)';
 
-$l['mybbfancybox_watermark_resolutions_title'] = 'Low resolution image dimensions';
-$l['mybbfancybox_watermark_resolutions_description'] = 'Enter the maximum dimensions in pixels that an image will be considered as low resolution<br />width|height eg. 300|300 (default)<br />(This setting has no affect if the above setting is set to NO)';
+$l['setting_mybbfancybox_watermark_resolutions'] = 'Low resolution image dimensions';
+$l['setting_mybbfancybox_watermark_resolutions_desc'] = 'Enter the maximum dimensions in pixels that an image will be considered as low resolution<br />width|height eg. 300|300 (default)<br />(This setting has no affect if the above setting is set to NO)';
 
-$l['mybbfancybox_per_post_gallery_title'] = 'Per-post gallery?';
-$l['mybbfancybox_per_post_gallery_description'] = 'YES (default) for each post to have its own gallery, NO for a global gallery for all posts on the page';
+$l['setting_mybbfancybox_per_post_gallery'] = 'Per-post gallery?';
+$l['setting_mybbfancybox_per_post_gallery_desc'] = 'YES (default) for each post to have its own gallery, NO for a global gallery for all posts on the page';
 
-$l['mybbfancybox_loop_title'] = 'Enable infinite gallery navigation?';
-$l['mybbfancybox_loop_description'] = 'YES (default) to enable infinite navigation between images in the gallery, NO to disable';
+$l['setting_mybbfancybox_loop'] = 'Enable infinite gallery navigation?';
+$l['setting_mybbfancybox_loop_desc'] = 'YES (default) to enable infinite navigation between images in the gallery, NO to disable';
 
-$l['mybbfancybox_infobar_title'] = 'Display image counter?';
-$l['mybbfancybox_infobar_description'] = 'YES (default) to display image counter in the top left corner, NO to hide';
+$l['setting_mybbfancybox_infobar'] = 'Display image counter?';
+$l['setting_mybbfancybox_infobar_desc'] = 'YES (default) to display image counter in the top left corner, NO to hide';
 
-$l['mybbfancybox_arrows_title'] = 'Display arrows for navigation?';
-$l['mybbfancybox_arrows_description'] = 'YES (default) to display arrows at the edges of the screen for simple navigation between images, NO to hide';
+$l['setting_mybbfancybox_arrows'] = 'Display arrows for navigation?';
+$l['setting_mybbfancybox_arrows_desc'] = 'YES (default) to display arrows at the edges of the screen for simple navigation between images, NO to hide';
 
-$l['mybbfancybox_thumbs_title'] = 'Display thumbnails sidebox?';
-$l['mybbfancybox_thumbs_description'] = 'YES to automatically display thumbnails sidebox, NO (default) to hide';
+$l['setting_mybbfancybox_thumbs'] = 'Display thumbnails sidebox?';
+$l['setting_mybbfancybox_thumbs_desc'] = 'YES to automatically display thumbnails sidebox, NO (default) to hide';
 
-$l['mybbfancybox_minimize_title'] = 'Enable minimize gallery?';
-$l['mybbfancybox_minimize_description'] = 'YES (default) to enable minimizing MyBB FancyBox galery, NO to disable';
+$l['setting_mybbfancybox_minimize'] = 'Enable minimize gallery?';
+$l['setting_mybbfancybox_minimize_desc'] = 'YES (default) to enable minimizing MyBB FancyBox galery, NO to disable';
 
 // Buttons
-$l['mybbfancybox_buttons_title'] = 'Button Selection';
-$l['mybbfancybox_buttons_description'] = 'Only selected buttons will display in the MyBB Fancybox modal';
+$l['setting_mybbfancybox_buttons'] = 'Button Selection';
+$l['setting_mybbfancybox_buttons_desc'] = 'Only selected buttons will display in the MyBB Fancybox modal';
 
-$l['mybbfancybox_button_slideshow_title'] = 'Slideshow';
-$l['mybbfancybox_button_fullscreen_title'] = 'Full Screen';
-$l['mybbfancybox_button_thumbs_title'] = 'Thumbnails';
-$l['mybbfancybox_button_share_title'] = 'Share';
-$l['mybbfancybox_button_download_title'] = 'Download';
-$l['mybbfancybox_button_zoom_title'] = 'Zoom';
-$l['mybbfancybox_button_close_title'] = 'Close';
+$l['setting_mybbfancybox_buttons_slideshow'] = 'Slideshow';
+$l['setting_mybbfancybox_buttons_fullscreen'] = 'Full Screen';
+$l['setting_mybbfancybox_buttons_thumbs'] = 'Thumbnails';
+$l['setting_mybbfancybox_buttons_share'] = 'Share';
+$l['setting_mybbfancybox_buttons_download'] = 'Download';
+$l['setting_mybbfancybox_buttons_zoom'] = 'Zoom';
+$l['setting_mybbfancybox_buttons_close'] = 'Close';
